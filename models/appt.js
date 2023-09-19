47da687a620bc29128c3ca82646910b007675c4c
AryanKum99 <aryan242003@gmail.com>
2023-09-20 03:50:38 +0530

    "dropdown"

--- models/appt.js
+++ models/appt.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const apptSchema = new Schema({
-    name:String,
-    user_email:String,
-    Select1:String,
-    Select2:String,
-    Test_DatetimeLocal:String
+    name: String,
+    userEmail: String,
+    Select1: String,
+    Select2: String,
+    apptTime: String
 });
 const Appt = mongoose.model('Appt', apptSchema);
 module.exports = Appt;
